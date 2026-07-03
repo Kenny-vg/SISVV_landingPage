a9fa02d9e9a1416f0cab17ff074252045f3c1bcc
mEzAcRiS <mezacristian2006@gmail.com>
2026-07-03 11:05:59 -0600

    Paleta de colores y tipografia

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Lato', ...defaultTheme.fontFamily.sans],
+                serif: ['Playfair Display', ...defaultTheme.fontFamily.serif],
+                alt: ['Montserrat', 'sans-serif'],
             },
         },
     },
