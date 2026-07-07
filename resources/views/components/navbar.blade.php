fa55e19f97691726cefada5e4dbacf245db64e73
mEzAcRiS <mezacristian2006@gmail.com>
2026-07-07 13:50:29 -0600

    Add page-load entrance animations (fade-in-up, fade-in)

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,11 @@
 <nav class="premium-navbar" id="main-navbar">
     <div class="navbar-container">
         <div class="navbar-left">
+            <button id="hamburger-btn" class="hamburger-btn" aria-label="Menú" title="Menú">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
             <ul class="navbar-links">
                 <li><a href="{{ url('/nosotros') }}">{{ setting('nav_link_nosotros', 'Nosotros') }}</a></li>
                 <li><a href="{{ url('/instalaciones') }}">{{ setting('nav_link_instalaciones', 'Instalaciones') }}</a></li>
@@ -22,13 +27,6 @@
                 <li><a href="{{ url('/lector') }}">{{ setting('nav_link_carta', 'Carta') }}</a></li>
                 <li><a href="{{ url('/#contacto') }}">{{ setting('nav_link_contacto', 'Contacto') }}</a></li>
             </ul>
-            <div class="navbar-actions">
-                <button id="hamburger-btn" class="hamburger-btn" aria-label="Menú" title="Menú">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-            </div>
         </div>
     </div>
 
