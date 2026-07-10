e38cec059f885bc641ee306169d361581541abfd
Aiman Delavega <aimankendra22@gmail.com>
2026-07-10 13:22:42 -0600

    fix: botón volver en términos y aviso de privacidad redirige al home

--- resources/views/aviso-privacidad.blade.php
+++ resources/views/aviso-privacidad.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div class="pdf-viewer-page">
 
-    <a href="{{ url('/nosotros') }}" class="pdf-back-btn" id="pdf-back-btn">
+    <a href="{{ url('/') }}" class="pdf-back-btn" id="pdf-back-btn">
         <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
         </svg>
